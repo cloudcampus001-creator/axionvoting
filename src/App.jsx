f152bd706756ff239d25a518ad405bc0c3f3cee1
CloudCampus <cloudcampus001@gmail.com>
2026-02-21 14:22:34 +0100

    rep

--- src/App.jsx
+++ src/App.jsx
@@ -227,7 +227,7 @@ function Login({ users, onLogin }) {
             Sign In
           </button>
         </form>
-        <p style={{ textAlign: "center", color: "#2d3748", fontSize: "12px", marginTop: "24px" }}>If fogotten your credentials <a href="https://wa.me/654840542">contact admin</a> </p>
+        <p style={{ textAlign: "center", color: "#2d3748", fontSize: "12px", marginTop: "24px" }}>If fogotten your credentials <button onClick={"https://wa.me/654840542"} style={{ background: "none", border: "none", color: "#b49650", cursor: "pointer", fontFamily: "Georgia,serif", fontSize: "12px" }}>contact admin</button> </p>
       </div>
     </div>
   );
